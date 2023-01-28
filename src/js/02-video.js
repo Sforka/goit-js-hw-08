798c4df8e418ab9cf1a9edd77c7606d3813615b1
Sforka <warlock20115@gmail.com>
2023-01-28 23:05:01 +0200

    1
    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,9 +2,7 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const VIDEOTIME_KEY = 'videoplayer-current-time'; 
-
-
+const VIDEOTIME_KEY = 'videoplayer-current-time';
 
 if (localStorage.getItem(VIDEOTIME_KEY)) {
   player.setCurrentTime(localStorage.getItem(VIDEOTIME_KEY));
@@ -16,4 +14,3 @@ player.on(
     localStorage.setItem(VIDEOTIME_KEY, event.seconds);
   }, 1000)
 );
-
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,21 +11,14 @@ const refs = {
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onformInput, 1000));
 
-
 function onFormSubmit(event) {
   event.preventDefault();
-    event.currentTarget.reset();
-    console.log('Send Data ' + localStorage.getItem(STORAGE_KEY));
-    localStorage.removeItem(STORAGE_KEY);
-    
-    
+  event.currentTarget.reset();
+  console.log('Send Data ' + localStorage.getItem(STORAGE_KEY));
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 function onformInput(event) {
-    formData[event.target.name] = [event.target.value];
-    console.log(formData)
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-    console.log(localStorage.getItem(STORAGE_KEY));
+  formData[event.target.name] = [event.target.value];
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
-
-
