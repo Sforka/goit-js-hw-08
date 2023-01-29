400c4a908f950ac17627a0b0cb567bccaa65faf6
Sforka <warlock20115@gmail.com>
2023-01-29 22:46:05 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,13 +4,11 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const VIDEOTIME_KEY = 'videoplayer-current-time';
 
-if (localStorage.getItem(VIDEOTIME_KEY)) {
-  player.setCurrentTime(localStorage.getItem(VIDEOTIME_KEY));
-}
 
-player.on(
-  'timeupdate',
-  throttle(function (event) {
+player.setCurrentTime(localStorage.getItem(VIDEOTIME_KEY)|| 0);
+
+
+player.on('timeupdate',throttle(function (event) {
     localStorage.setItem(VIDEOTIME_KEY, event.seconds);
   }, 1000)
 );
