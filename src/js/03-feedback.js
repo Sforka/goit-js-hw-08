08c9b1da383155d0eceeaf6de27dfab01b3734a9
Sforka <warlock20115@gmail.com>
2023-01-29 16:14:41 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,21 +4,30 @@ const formData = {};
 
 const refs = {
   form: document.querySelector('.feedback-form'),
-  email: document.querySelector('.feedback-form email'),
+  email: document.querySelector('.feedback-form input'),
   textarea: document.querySelector('.feedback-form textarea'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onformInput, 1000));
+if (localStorage.getItem(STORAGE_KEY)) {
+  const saveData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (saveData.message != undefined) refs.textarea.value = saveData.message || '';
+  if (saveData.email != undefined) refs.email.value = saveData.email || '';
+}
 
 function onFormSubmit(event) {
   event.preventDefault();
+   if (event.target.elements.email.value === "" || event.target.elements.message.value === "") { 
+        return alert("Увага! Всі поля повинні бути заповнені!"); 
+    };  
   event.currentTarget.reset();
   console.log('Send Data ' + localStorage.getItem(STORAGE_KEY));
   localStorage.removeItem(STORAGE_KEY);
 }
 
 function onformInput(event) {
+  console.log(event.target);
   formData[event.target.name] = [event.target.value];
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
